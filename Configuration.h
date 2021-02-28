5d9e8b0cfcb0e940253c1860a06166f43617c9e0
Will Cooke <wrcooke@wrcooke.net>
2021-02-28 09:47:42 -0600

    Added configuration and more symbol table stuff

---
+++ Configuration.h
@@ -0,0 +1,20 @@
+/*
+ * Configuration.h
+ *
+ *  Created on: Feb 28, 2021
+ *      Author: wrcooke
+ */
+
+#ifndef CONFIGURATION_H_
+#define CONFIGURATION_H_
+
+// Symbol
+
+#define SYM_MAX_LENGTH      31
+
+typedef uint16_t Value_t;
+
+
+
+
+#endif /* CONFIGURATION_H_ */
--- SymTable.h
+++ SymTable.h
@@ -8,6 +8,12 @@
 #ifndef SYMTABLE_H_
 #define SYMTABLE_H_
 
+#include <stdint.h>
+#include <string.h>
+
+
+#include "Configuration.h"
+
 typedef enum SYMTYPE
 {
 	SYM_UNDEF      = 0,
@@ -21,8 +27,17 @@ typedef enum SYMTYPE
 class Symbol
 {
 public:
+	Symbol(char* nm, SymbolType_t t, Value_t t = 0, bool d = false)
+{
+		strncpy(name, nm, SYM_MAX_LENGTH);
+}
 
 private:
+	char name[SYM_MAX_LENGTH + 1];
+	SymbolType_t typ;
+	Value_t      val;
+	bool         defined;
+
 
 };
 
@@ -30,8 +45,8 @@ private:
 class SymbolTable
 {
 public:
-	SymbolTabel();
-	SymbolType_t getType();
+	SymbolTable();
+	SymbolType_t getType(char *name);
 private:
 
 };
